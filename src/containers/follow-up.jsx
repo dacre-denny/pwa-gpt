3816948f62b180f47f819e57ba8ae3061058e451
Dacre <enquiries@mooce.co.nz>
2018-04-14 21:45:29 +1200

    introduce caching of some assets to service worker

--- src/containers/follow-up.jsx
+++ src/containers/follow-up.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import * as actions from '../store/actions'
-import * as API from '../api'
+import * as actions from "../store/actions";
+import * as API from "../api";
 
-const component = ({ followUps, deleteFollowUp }) => (<div>
-  <div className="content">
-    <figure>Follow up</figure>
-    <ul>
-      {
-        followUps.map(({ name, contact, id }) => <li key={id}>
-          <div>
-            <h3>{ name }</h3>
-            <h5>{ contact }</h5>
-          </div>
-          <button onClick={ () => deleteFollowUp(id) }>Delete</button>
-        </li>)
-      }
-    </ul>
+const component = ({ followUps, deleteFollowUp }) => (
+  <div>
+    <div>
+      <Link to="/" className="mdi mdi-back">
+        Home
+      </Link>
+      <h1>Follow Ups</h1>
+      <h3>Manage and contact follow ups from prior witnessing</h3>
+    </div>
+    <div className="content scroll"> 
+      <ul className="follow-up">
+        {followUps.map(({ name, date, contact, id }) => (
+          <li key={id}>
+            <div>
+              <h3>{name}</h3>
+              <small>{ date }</small>
+              <p>{contact}</p>
+            </div>
+            <a className="delete mdi mdi-close" onClick={() => deleteFollowUp(id)}></a>
+          </li>
+        ))}
+      </ul>
+    </div>
   </div>
-  <div className="footer btn-group">
-    <Link to='/' className="btn">Home</Link>
-  </div>
-</div>)
+);
 
-export default connect(state => ({
-  followUps: API.getFollowUps()
-}), dispatch => ({
-  deleteFollowUp : (id) => dispatch(actions.deleteFollowUp(id))
-}))(component)
+export default connect(
+  state => ({
+    followUps: API.getFollowUps()
+  }),
+  dispatch => ({
+    deleteFollowUp: id => dispatch(actions.deleteFollowUp(id))
+  })
+)(component);
